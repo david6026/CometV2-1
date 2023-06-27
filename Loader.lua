1b6fcc57652851ddaf4172ee255fcd13f5cdf11a
david????#6026 <113543570+david6026@users.noreply.github.com>
2023-06-27 23:41:46 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,26 +9,38 @@ if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
     if betterisfile("CometV2/Modules/Bedwars.lua") then
         loadstring(readfile("CometV2/Modules/Bedwars.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Bedwars.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/david6026/CometV2/main/Modules/Bedwars.lua"))()
     end
 elseif placeid == 6872265039 then
     if betterisfile("CometV2/Modules/6872265039.lua") then
         loadstring(readfile("CometV2/Modules/6872265039.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/6872265039.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/david6026/CometV2/main/Modules/6872265039.lua"))()
     end
 else
     if betterisfile("CometV2/Modules/Universal.lua") then
         loadstring(readfile("CometV2/Modules/Universal.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Universal.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/david6026/CometV2/main/Modules/Universal.lua"))()
     end
 end
 
 task.delay(1, function()
     game:GetService("StarterGui"):SetCore("ChatWindowPosition", UDim2.new(0,0,0.73,0))
     game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
-        Text = "Comet V2 Loaded!",
+        Text = "Better Comet V2 Loaded!",
+        Color = Color3.fromRGB(255,65,65),
+        Font = Enum.Font.SourceSansBold,
+    })
+end)
+
+
+task.delay(1, function()
+    game:GetService("StarterGui"):SetCore("ChatWindowPosition", UDim2.new(0,0,0.73,0))
+    game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+        Text = "ChangeLogs :
+                [+]Added new killaura animations (abt 4 or 5)
+                when other updates release i will add more since now im lazy",
         Color = Color3.fromRGB(255,65,65),
         Font = Enum.Font.SourceSansBold,
     })
